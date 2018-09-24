0a813af2098912355c9176ca7980df09f041cf2e
Jorge Murillo <jemurillo@correo.url.edu.gt>
2018-09-23 21:27:59 -0600

    metodos para agregar, leer y eliminar en el servicio

--- lab2/src/app/services/personservices.service.ts
+++ lab2/src/app/services/personservices.service.ts
@@ -1,9 +1,54 @@
 import { Injectable } from '@angular/core';
-
+import { Persona } from '../models/mymain';
 @Injectable({
   providedIn: 'root'
+
 })
 export class PersonservicesService {
+ personas: Persona[];
 
-  constructor() { }
+  constructor() {
+    this.personas = [];
+   }
+
+   getPersona(): Persona[] {
+     if (localStorage.getItem('personas') === null) {
+       this.personas = [];
+     } else {
+       this.personas = JSON.parse(localStorage.getItem('personas'));
+     }
+     return this.personas;
+   }
+
+   addPerson(person: Persona): void {
+      this.personas.unshift(person);
+      if (localStorage.getItem('personas') === null) {
+        this.personas = [];
+        this.personas.unshift(person);
+        localStorage.setItem('personas', JSON.stringify(this.personas) );
+      } else {
+        this.personas = JSON.parse(localStorage.getItem('personas'));
+        this.personas.unshift(person);
+        localStorage.setItem('personas', JSON.stringify(this.personas) );
+      }
+   }
+
+   deletePerson(person: Persona) {
+     for (let i = 0; i < this.personas.length; i++) {
+       if (person === this.personas[i]) {
+         this.personas.splice(i, 1);
+         localStorage.setItem('personas', JSON.stringify(this.personas) );
+       }
+     }
+    this.personas.unshift(person);
+    if (localStorage.getItem('personas') === null) {
+      this.personas = [];
+      this.personas.unshift(person);
+      localStorage.setItem('personas', JSON.stringify(this.personas) );
+    } else {
+      this.personas = JSON.parse(localStorage.getItem('personas'));
+      this.personas.unshift(person);
+      localStorage.setItem('personas', JSON.stringify(this.personas) );
+    }
+ }
 }
